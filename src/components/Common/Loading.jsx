407b242b168329b7f5e3cbfccc26aa5d1d9412d2
komal raj <komalraj@komals-MacBook-Pro.local>
2023-11-27 19:58:09 +0530

    Add common loading component

---
+++ src/components/Common/Loading.jsx
@@ -0,0 +1,17 @@
+const Loading = ({ additionalMessage = "" }) => {
+  return (
+    <div>
+      <div class="lds-facebook">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      {
+        // if additionalMessage is present, then we are showing the additional message
+        additionalMessage && <div>{additionalMessage}</div>
+      }
+    </div>
+  );
+};
+
+export default Loading;
