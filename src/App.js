be05877a8d077f41b8599d3415f16880d3e20fd4
komal raj <komalraj@komals-MacBook-Pro.local>
2023-11-27 20:37:38 +0530

    Add news details page route

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import Header from "./components/Header";
 import "./App.css";
 
 const Main = lazy(() => import("./components/Main"));
+const NewsDetails = lazy(() => import("./components/NewsDetails"));
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    element: <Main />,
+  },
+  {
+    path: "/news/:id",
+    name: "News Details",
+    element: <NewsDetails />,
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Header />
+        <Header />
         <Routes>
-          <Route path="/" element={<Main />} />
+          {routes?.map(({ path, name, element }) => (
+            <Route key={name} path={path} element={element} />
+          ))}
         </Routes>
       </Router>
     </div>
